f221525b4c8641dd5c3ce3fa702eeb1057935698
Vaibhav Bajaj <97230062+v-aibhavbajaj2520@users.noreply.github.com>
2024-08-17 18:44:04 +0530

    Update Projects.js
    Changed the name to theYukt

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,9 +23,9 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={theYukt}
               isBlog={false}
-              title="Chatify"
+              title="theYukt"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/soumyajit4419/Chatify"
               demoLink="https://chatify-49.web.app/"
