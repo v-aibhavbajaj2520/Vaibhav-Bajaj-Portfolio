e9cab4ed5aabbbaa884c20c35101d71300d1a21b
Vaibhav Bajaj <97230062+v-aibhavbajaj2520@users.noreply.github.com>
2024-11-28 15:57:32 +0530

    Update Navbar.js
    changed the github account link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/v-aibhavbajaj2520/Portfolio"
+                href="https://github.com/v-aibhavbajaj2520/Vaibhav-Bajaj-Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
